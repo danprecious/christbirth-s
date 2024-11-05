e56b2755775d3340fe348bae7b0be884d91f726f
Kayode Dan Precious <danielkayode222@gmail.com>
2024-11-05 17:47:20 +0100

    main commit

---
+++ src/app/page.js
@@ -0,0 +1,36 @@
+"use client";
+import {} from 'react-icons/bi'
+
+const HomePage = () => {
+  return (
+    <section className="lg:px-20 px-6 py-10 h-[100vh]">
+      <div className="text-amber-950 mb-5">
+        <h1 className="text-[1.2em] mb-5 font-semibold"> ABRAHAM  <br></br> Et <br></br> CHRISTBIRTH</h1>
+        <div className="w-full  bg-stone-900 h-[50vh]"></div>
+
+        <div className="mt-5 text-sm">
+          <p className="my-3">
+            Dearest <strong> Abraham,</strong>
+          </p>
+          <p className="">
+            It is my greatest pleasure to write this to you. In you, I've found
+            peace and joy; you've been a profound influence in my life, my alter
+            ego. You are a very special friend that i cherish. I just want to
+            wish you a very happy birthday and pray that this day brings you
+            endless joy and blessings. I love you so much babe, and I pray God
+            keeps us together through any trials we may face. God bless you
+            always.{" "}
+          </p>
+          <div className="my-3">
+            <p className="">With all my love,</p>
+            <p className="">
+              <strong>Christbirth</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
